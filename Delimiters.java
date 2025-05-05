5bc021d7397048c830edcde7219a63f6ee0c15d4
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2025-05-05 01:33:14 +0000

    done

--- Delimiters.java
+++ Delimiters.java
@@ -1,28 +1,47 @@
-java.util.ArrayList;
+import java.util.ArrayList;
 public class Delimiters
 {
-/** The open and close delimiters. */
+
 private String openDel;
 private String closeDel;
-/** Constructs a Delimiters object where open is the open delimiter and close is the
-* close delimiter.
-* Precondition: open and close are non-empty strings.
-*/
+
 public Delimiters(String open, String close)
 {
 openDel = open;
 closeDel = close;
 }
-/** Returns an ArrayList of delimiters from the array tokens, as described in part (a). */
+
 public ArrayList<String> getDelimitersList(String[] tokens)
 {
-    
-    for(int i)
+    ArrayList<String> delims = new ArrayList<String>();
+    for (String str : tokens)
+    {
+        if (str.equals(openDel) || str.equals(closeDel))
+        {
+            delims.add(str);
+        }   
+    }
+    return delims;
 }
-/** Returns true if the delimiters are balanced and false otherwise, as described in part (b).
-* Precondition: delimiters contains only valid open and close delimiters.
-*/
+
 public boolean isBalanced(ArrayList<String> delimiters)
-{ /* to be implemented in part (b) */ }
-// There may be instance variables, constructors, and methods that are not shown.
+{
+    int opencount = 0;
+    int closecount = 0;
+    for (String str : delimiters)
+    {
+        if (str.equals(openDel))
+        {
+            opencount++;
+        }  
+        if (str.equals(closeDel))
+        {
+            closecount++;
+        }    
+        if(closecount > opencount) return false;
+
+    }
+    return closecount == opencount;
+}
+
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        Delimiters e1 = new Delimiters("(", ")");
+        String[] t1 = {"(", "x + y", ")"," * 5"};
+        System.out.println(e1.getDelimitersList(t1));
+        Delimiters e2 = new Delimiters("<q>", "</q>");
+        String[] t2 = {"<q>", "yy", "</q>","zz", "</q>"};
+        System.out.println(e2.getDelimitersList(t2));
+        Delimiters e3 = new Delimiters("<sup>", "</sup>");
+        String[] t3 = {"<sup>", "<sup>", "</sup>","<sup>", "</sup>", "</sup>"};
+        System.out.println(e3.isBalanced(e3.getDelimitersList(t3)));
+        String[] t4 = {"<sup>", "</sup>", "</sup>","<sup>"};
+        System.out.println(e3.isBalanced(e3.getDelimitersList(t4)));
+        String[] t5 = {"</sup>"};
+        System.out.println(e3.isBalanced(e3.getDelimitersList(t5)));
+        String[] t6 = {"<sup>", "<sup>", "</sup>"};
+        System.out.println(e3.isBalanced(e3.getDelimitersList(t6)));
+
     }
 }
